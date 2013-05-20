0b2ed44af131668889fee4844f6a56fbdd1185c2
Sicco de Vries <sdvries2906@gmail.com>
2013-05-20 11:20:33 +0200

    Added base templates

--- mysite/settings.py
+++ mysite/settings.py
@@ -111,6 +111,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    '/Users/sdvries/dev/mysite/templates',
 )
 
 INSTALLED_APPS = (
@@ -120,10 +121,8 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admin',
+    'django.contrib.admindocs',
     'south',
     'polls',
 )
